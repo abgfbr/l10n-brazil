b526b4b6eab75caae4313a20df66236940229e6b
Wagner Pereira <wagner.pereira@abgf.gov.br>
2018-06-21 08:09:35 -0300

    [ADD] Fazer EFD/Reinf usar o novo campo 'ambiente' no registro de transmissão

--- sped_efdreinf/models/sped_efdreinf.py
+++ sped_efdreinf/models/sped_efdreinf.py
@@ -407,6 +407,7 @@ class SpedEfdReinf(models.Model):
                 values = {
                     'tipo': 'efdreinf',
                     'registro': 'R-2010',
+                    'ambiente': self.company_id.tpAmb,
                     'company_id': self.company_id.id,
                     'evento': 'evtServTom',
                     'origem': ('sped.efdreinf.estabelecimento,%s' % estabelecimento.id),
@@ -424,6 +425,7 @@ class SpedEfdReinf(models.Model):
             values = {
                 'tipo': 'efdreinf',
                 'registro': 'R-2099',
+                'ambiente': self.company_id.tpAmb,
                 'company_id': self.company_id.id,
                 'evento': 'evtFechamento',
                 'origem': ('sped.efdreinf,%s' % efdreinf.id),
