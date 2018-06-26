be07ba49f4f2fd3d484aab21056fb1f45d9110ee
Wagner Pereira <wagner.pereira@abgf.gov.br>
2018-06-26 11:19:04 -0300

    [FIX] Eliminar letras maiúsculas de nome de campos sped (causa problemas no postgres)

--- sped_esocial/models/sped_esocial.py
+++ sped_esocial/models/sped_esocial.py
@@ -86,7 +86,7 @@ class SpedEsocial(models.Model):
     #
     #         # Checa se algum registro já foi transmitido
     #         for estabelecimento in esocial.estabelecimento_ids:
-    #             if estabelecimento.requer_S1005 and estabelecimento.situacao_S1005 in ['2', '4']:
+    #             if estabelecimento.requer_s1005 and estabelecimento.situacao_s1005 in ['2', '4']:
     #                 raise ValidationError("Não pode excluir um Período e-Social que já tem algum processamento!")
 
     @api.depends('estabelecimento_ids')
@@ -209,10 +209,10 @@ class SpedEsocial(models.Model):
             self.cargo_ids = [(4, cargo_id.id)]
 
     @api.multi
-    def criar_S1005(self):
+    def criar_s1005(self):
         self.ensure_one()
         for estabelecimento in self.estabelecimento_ids:
-            if not estabelecimento.sped_S1005_registro:
+            if not estabelecimento.sped_s1005_registro:
 
                 # Criar registro
                 values = {
@@ -223,14 +223,14 @@ class SpedEsocial(models.Model):
                     'evento': 'evtTabEstab',
                     'origem': ('sped.esocial.estabelecimento,%s' % estabelecimento.id),
                 }
-                sped_S1005_registro = self.env['sped.transmissao'].create(values)
-                estabelecimento.sped_S1005_registro = sped_S1005_registro
+                sped_s1005_registro = self.env['sped.transmissao'].create(values)
+                estabelecimento.sped_s1005_registro = sped_s1005_registro
 
     @api.multi
-    def criar_S1010(self):
+    def criar_s1010(self):
         self.ensure_one()
         for rubrica in self.rubrica_ids:
-            if not rubrica.sped_S1010_registro:
+            if not rubrica.sped_s1010_registro:
 
                 # Criar registro
                 values = {
@@ -241,14 +241,14 @@ class SpedEsocial(models.Model):
                     'evento': 'evtTabRubrica',
                     'origem': ('sped.esocial.rubrica,%s' % rubrica.id),
                 }
-                sped_S1010_registro = self.env['sped.transmissao'].create(values)
-                rubrica.sped_S1010_registro = sped_S1010_registro
+                sped_s1010_registro = self.env['sped.transmissao'].create(values)
+                rubrica.sped_s1010_registro = sped_s1010_registro
 
     @api.multi
-    def criar_S1020(self):
+    def criar_s1020(self):
         self.ensure_one()
         for lotacao in self.lotacao_ids:
-            if not lotacao.sped_S1020_registro:
+            if not lotacao.sped_s1020_registro:
 
                 # Criar registro
                 values = {
@@ -259,14 +259,14 @@ class SpedEsocial(models.Model):
                     'evento': 'evtTabLotacao',
                     'origem': ('sped.esocial.lotacao,%s' % lotacao.id),
                 }
-                sped_S1020_registro = self.env['sped.transmissao'].create(values)
-                lotacao.sped_S1020_registro = sped_S1020_registro
+                sped_s1020_registro = self.env['sped.transmissao'].create(values)
+                lotacao.sped_s1020_registro = sped_s1020_registro
 
     @api.multi
-    def criar_S1030(self):
+    def criar_s1030(self):
         self.ensure_one()
         for cargo in self.cargo_ids:
-            if not cargo.sped_S1030_registro:
+            if not cargo.sped_s1030_registro:
 
                 # Criar registro
                 values = {
@@ -277,5 +277,5 @@ class SpedEsocial(models.Model):
                     'evento': 'evtTabCargo',
                     'origem': ('sped.esocial.cargo,%s' % cargo.id),
                 }
-                sped_S1030_registro = self.env['sped.transmissao'].create(values)
-                cargo.sped_S1030_registro = sped_S1030_registro
+                sped_s1030_registro = self.env['sped.transmissao'].create(values)
+                cargo.sped_s1030_registro = sped_s1030_registro
